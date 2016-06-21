4331cb58a57ef3ecf0174e7b0031cda861173f2a
Michael Remijan <mjremijan@yahoo.com>
2016-06-21 09:18:27 -0500

    change spacing for blog

--- src/test/java/org/thoth/jpa/UnitTesting/Person.java
+++ src/test/java/org/thoth/jpa/UnitTesting/Person.java
@@ -11,60 +11,47 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
- *
  * @author Michael Remijan mjremijan@yahoo.com @mjremijan
- * 
- * Section 3.3
- * https://www.javacodegeeks.com/2015/02/jpa-tutorial.html#relationships_onetomany
  */
 @Entity
 @Table(name = "T_PERSON")
-public class Person {
-    
-	private Long id;
-	private String firstName;
-	private String lastName;
-    private List<Phone> phones = new ArrayList<>();
-    
-	@Id
-	@GeneratedValue()
-	public Long getId() {
-		return id;
-	}
+  public class Person {
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private List<Phone> phones = new ArrayList<>();
 
-	@Column(name = "FIRST_NAME")
-	public String getFirstName() {
-		return firstName;
-	}
+  @Id
+  @GeneratedValue()
+  public Long getId() {
+    return id;
+  }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	@Column(name = "LAST_NAME")
-	public String getLastName() {
-		return lastName;
-	}
+  @Column(name = "FIRST_NAME")
+  public String getFirstName() {
+    return firstName;
+  }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-    
-   /*
-    * On the other side of the relation we have to add a List of Phone 
-    * objects to the person and annotate the corresponding getter method 
-    * with @OneToMany as we have “one” person with “many” phones
-    *
-    * The value of the attribute mappedBy tells JPA which list on the other 
-    * side of the relation (here Phone.person) this annotation references.    
-    */
-    @OneToMany(mappedBy = "person", fetch = FetchType.LAZY)
-    public List<Phone> getPhones() {
-        return phones;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  @Column(name = "LAST_NAME")
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  @OneToMany(mappedBy = "person", fetch = FetchType.LAZY)
+  public List<Phone> getPhones() {
+    return phones;
+  }
 }
-
--- src/test/java/org/thoth/jpa/UnitTesting/PersonTest.java
+++ src/test/java/org/thoth/jpa/UnitTesting/PersonTest.java
@@ -12,105 +12,112 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- *
  * @author Michael Remijan mjremijan@yahoo.com @mjremijan
  */
 public class PersonTest {
-
-    @Test
-    public void typeAnnotations() {
-        // assert
-        AssertAnnotations.assertType(
-            Person.class, new Class[]{Entity.class, Table.class});
-    }
-
-
-    @Test
-    public void fieldAnnotations() {
-        // assert
-        AssertField.assertFields(Person.class, "id", "firstName", "lastName", "phones");
-        AssertAnnotations.assertField(Person.class, "id");
-        AssertAnnotations.assertField(Person.class, "firstName");
-        AssertAnnotations.assertField(Person.class, "lastName");
-        AssertAnnotations.assertField(Person.class, "phones");
-    }
+  @Test
+  public void typeAnnotations() {
+    // assert
+    AssertAnnotations.assertType(
+        Person.class, Entity.class, Table.class);
+  }
 
 
-    @Test
-    public void methodAnnotations() {
-        // assert
-        AssertAnnotations.assertMethod(Person.class, "getId", Id.class, GeneratedValue.class);
-        AssertAnnotations.assertMethod(Person.class, "getFirstName", Column.class);
-        AssertAnnotations.assertMethod(Person.class, "getLastName", Column.class);
-        AssertAnnotations.assertMethod(Person.class, "getPhones", OneToMany.class);
-    }
+  @Test
+  public void fieldAnnotations() {
+    // assert
+    AssertAnnotations.assertField(Person.class, "id");
+    AssertAnnotations.assertField(Person.class, "firstName");
+    AssertAnnotations.assertField(Person.class, "lastName");
+    AssertAnnotations.assertField(Person.class, "phones");
+  }
 
 
-    @Test
-    public void entity() {
-        // setup
-        Entity a
-            = ReflectTool.getClassAnnotation(Person.class, Entity.class);
+  @Test
+  public void methodAnnotations() {
+    // assert
+    AssertAnnotations.assertMethod(
+        Person.class, "getId", Id.class, GeneratedValue.class);
 
-        // assert
-        Assert.assertEquals("", a.name());
-    }
+    AssertAnnotations.assertMethod(
+        Person.class, "getFirstName", Column.class);
 
+    AssertAnnotations.assertMethod(
+        Person.class, "getLastName", Column.class);
 
-    @Test
-    public void table() {
-        // setup
-        Table t
-            = ReflectTool.getClassAnnotation(Person.class, Table.class);
-
-        // assert
-        Assert.assertEquals("T_PERSON", t.name());
-    }
+    AssertAnnotations.assertMethod(
+        Person.class, "getPhones", OneToMany.class);
+  }
 
 
-    @Test
-    public void id() {
-        // setup
-        GeneratedValue a
-            = ReflectTool.getMethodAnnotation(Person.class, "getId", GeneratedValue.class);
+  @Test
+  public void entity() {
+    // setup
+    Entity a
+    = ReflectTool.getClassAnnotation(Person.class, Entity.class);
 
-        // assert
-        Assert.assertEquals("", a.generator());
-        Assert.assertEquals(GenerationType.AUTO, a.strategy());
-    }
-
-
-    @Test
-    public void firstName() {
-        // setup
-        Column c
-            = ReflectTool.getMethodAnnotation(Person.class, "getFirstName", Column.class);
-
-        // assert
-        Assert.assertEquals("FIRST_NAME", c.name());
-    }
+    // assert
+    Assert.assertEquals("", a.name());
+  }
 
 
-    @Test
-    public void lastName() {
-        // setup
-        Column c
-            = ReflectTool.getMethodAnnotation(Person.class, "getLastName", Column.class);
+  @Test
+  public void table() {
+    // setup
+    Table t
+    = ReflectTool.getClassAnnotation(Person.class, Table.class);
 
-        // assert
-        Assert.assertEquals("LAST_NAME", c.name());
-    }
+    // assert
+    Assert.assertEquals("T_PERSON", t.name());
+  }
 
 
-    @Test
-    public void phones() {
-        // setup
-        OneToMany a
-            = ReflectTool.getMethodAnnotation(Person.class, "getPhones", OneToMany.class);
+  @Test
+  public void id() {
+    // setup
+    GeneratedValue a
+    = ReflectTool.getMethodAnnotation(
+        Person.class, "getId", GeneratedValue.class);
 
-        // assert
-        Assert.assertEquals("person", a.mappedBy());
-        Assert.assertEquals(FetchType.LAZY, a.fetch());
-    }
+    // assert
+    Assert.assertEquals("", a.generator());
+    Assert.assertEquals(GenerationType.AUTO, a.strategy());
+  }
 
+
+  @Test
+  public void firstName() {
+    // setup
+    Column c
+    = ReflectTool.getMethodAnnotation(
+        Person.class, "getFirstName", Column.class);
+
+    // assert
+    Assert.assertEquals("FIRST_NAME", c.name());
+  }
+
+
+  @Test
+  public void lastName() {
+    // setup
+    Column c
+    = ReflectTool.getMethodAnnotation(
+        Person.class, "getLastName", Column.class);
+
+    // assert
+    Assert.assertEquals("LAST_NAME", c.name());
+  }
+
+
+  @Test
+  public void phones() {
+    // setup
+    OneToMany a
+    = ReflectTool.getMethodAnnotation(
+        Person.class, "getPhones", OneToMany.class);
+
+    // assert
+    Assert.assertEquals("person", a.mappedBy());
+    Assert.assertEquals(FetchType.LAZY, a.fetch());
+  }
 }
